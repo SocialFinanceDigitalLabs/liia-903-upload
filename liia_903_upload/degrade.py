4f57cb179ed5cacf594889e16914a1615fbadd77
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2022-03-31 15:45:35 +0300

    more comments

--- liia_903_upload/degrade.py
+++ liia_903_upload/degrade.py
@@ -7,6 +7,9 @@ from liia_903_upload.converters import to_short_postcode, to_month_only_dob
 
 @streamfilter(check=type_check(StartElement), fail_function=pass_event, error_function=pass_event)
 def degrade_postcodes(event):
+    """
+    Convert all values that should be postcodes to shorter postcodes
+    """
     if event.label == "HOME_POST":
         text = to_short_postcode(event.text)
     elif event.label == "PL_POST":
@@ -18,12 +21,18 @@ def degrade_postcodes(event):
 
 @streamfilter(check=type_check(StartElement), fail_function=pass_event, error_function=pass_event)
 def degrade_dob(event):
+    """
+    Convert all values that should be dates of birth to months and year of birth
+    """
     dob = event.config["DOB"]
     text = to_month_only_dob(event.text, dob)
     return event.from_event(event, text=text)
 
 
 def degrade(stream):
+    """
+    Compile the degrading functions
+    """
     stream = degrade_postcodes(stream)
     stream = degrade_dob(stream)
     return stream
